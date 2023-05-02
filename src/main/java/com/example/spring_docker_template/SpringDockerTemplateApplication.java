b80bc3b64d6c17186f53916afc716738ad314104
AnatolSalau <salovanatoly5@mail.ru>
2023-05-02 21:05:41 +0300

    Initial commit

---
+++ src/main/java/com/example/spring_docker_template/SpringDockerTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_docker_template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDockerTemplateApplication {
+
+      public static void main(String[] args) {
+            SpringApplication.run(SpringDockerTemplateApplication.class, args);
+      }
+
+}
